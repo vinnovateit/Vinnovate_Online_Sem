2ff1e0c09ba4bdcdf72b56f4f5935d2770871592
wimpywarlord <wimpywarlord@gmail.com>
2020-07-10 23:58:33 +0530

    completed searching

--- app.js
+++ app.js
@@ -13,6 +13,41 @@ app.get("/", function (req, res) {
     });
 });
 
+app.post("/results", function (req, res) {
+    var searched_subjects = [
+        {
+            "Sl.No": "410",
+            Subject: "Web mining",
+            "Faculty name": "Shashank mouli satapathy",
+            Slot: "F1",
+            "Whatsapp Link": "https://chat.whatsapp.com/HjyBmdmmbrw19Vwr0YfFDf",
+            Telegram: "",
+        },
+    ];
+    // console.log(req.body.subject);
+    allGroups.forEach((element) => {
+        var subject = element.Subject.toLowerCase();
+        var teacher = element["Faculty name"].toLowerCase();
+        var slot = element["Slot"].toLowerCase();
+        var search = req.body.subject.toLowerCase();
+        // console.log(subject);
+        // console.log(search);
+        // console.log(subject.includes(search));
+        if (
+            subject.includes(search) ||
+            teacher.includes(search) ||
+            slot.includes(search)
+        ) {
+            searched_subjects.unshift(element);
+            // console.log("%%%%%%%%%%%%%%%%%%%%%%");
+        }
+    });
+    console.log(searched_subjects);
+    res.render("index.ejs", {
+        text: searched_subjects,
+    });
+});
+
 app.listen(process.env.PORT || 8000, function () {
     console.log("SERVER 8000 HAS STARTED");
 });
